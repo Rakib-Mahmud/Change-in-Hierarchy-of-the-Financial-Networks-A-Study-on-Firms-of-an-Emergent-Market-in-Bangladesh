ed088c386cc616d49f8e4a8933fcaefb6669c78e
Rakib Mahmud <31930455+Rakib-Mahmud@users.noreply.github.com>
2022-09-30 21:53:52 +0600

    Using Hierarchical Path

--- Hierarchy_Measurment.py
+++ Hierarchy_Measurment.py
@@ -5,10 +5,23 @@ import networkx as nx
 import networkx.algorithms.community as nx_comm
 import matplotlib.pyplot as plt
 
-#Function to check node-orders based on degree
-def isSorted(arr):
-    if (all(arr[i] <= arr[i+1] for i in range(len(arr)-1))) or (all(arr[i] >= arr[i+1] for i in range(len(arr)-1))):
+#Function to check node-orders based on degree to determine if the path is hierarchical
+def isHierarchical(path):
+    #If an up-path or down-path exist, the path is hierarchical
+    if (all(path[i] <= path[i+1] for i in range(len(path)-1))) or (all(path[i] >= path[i+1] for i in range(len(path)-1))):
         return True
+    #If an up-path followed by a down-path exist, the path is hierarchical
+    i = 0
+    last_idx = len(path)-1
+    if i<last_idx and path[i]<=path[i+1]:
+        while i<last_idx and path[i]<=path[i+1]:
+            i+=1
+        if i<last_idx and path[i]>=path[i+1]:
+            while i<last_idx and path[i]>=path[i+1]:
+                i+=1
+    if i == last_idx:
+        return True
+    
     return False   
 
 #Function to calculate the hierarchy based on the 'hierarchical path' concept
@@ -20,7 +33,7 @@ def hierarchy(mst):
             paths = nx.all_simple_paths(mst, source=i, target=j)
             for path in paths:
                 deg = [degree[m] for m in path]
-                if isSorted(deg):
+                if isHierarchical(deg):
                     cnt += 1
     n = len(mst.nodes)
     F = cnt/((n*(n-1))//2)
